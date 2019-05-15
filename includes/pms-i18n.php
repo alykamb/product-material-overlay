f01321a63db536c6f2e24201d057ee59b2dbd172
alykamb <34348520+alykamb@users.noreply.github.com>
2019-05-15 09:35:51 -0300

    Initial Commit

---
+++ includes/pms-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       https://github.com/alykamb
+ * @since      1.0.0
+ *
+ * @package    Product_Material_Selection
+ * @subpackage Product_Material_Selection/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Product_Material_Selection
+ * @subpackage Product_Material_Selection/includes
+ * @author     Alykam Burdzaki <alykam@gmail.com>
+ */
+class Product_Material_Selection_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'plugin-name',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
